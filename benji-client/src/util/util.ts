b3152fb37663b219dbf7838c198393a5c7248851
Danilo Arantes <dfilho@purdue.edu>
2021-11-03 21:11:33 -0300

    added hours to uptime counter

--- benji-client/src/util/util.ts
+++ benji-client/src/util/util.ts
@@ -1,31 +1,43 @@
 import { parse } from 'tinyduration';
 
 export type Duration = {
-  [index: string]: number | undefined,
-  years?: number,
-  months?: number,
-  days?: number,
-  hours?: number,
-  minutes?: number,
-  seconds?: number,
-}
+  [index: string]: number | undefined;
+  years?: number;
+  months?: number;
+  days?: number;
+  hours?: number;
+  minutes?: number;
+  seconds?: number;
+};
 
 export const msToMinSec = (ms: number) => {
-  const minutes: number = Math.floor(ms / 60000);
-  const seconds: number = Number(((ms % 60000) / 1000).toFixed(0));
-  return seconds === 60
-    ? minutes + 1 + ':00'
-    : minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+  let hours = Math.floor(ms / 3600000);
+  let minutes: number = Math.floor((ms % 3600000) / 60000);
+  let seconds: number = Number((((ms % 3600000) % 60000) / 1000).toFixed(0));
+
+  if (seconds === 60) {
+    minutes++;
+    seconds -= 60;
+  }
+
+  if (minutes >= 60) {
+    minutes -= 60;
+    hours += 1;
+  }
+
+  return `${hours ? hours + ':' : ''}${
+    minutes ? (minutes < 10 ? '0' : '') + minutes : '00'
+  }:${seconds ? (seconds < 10 ? '0' : '') + seconds : '00'}`;
 };
 
 export const convertISODurationToMS = (isoDuration: string) => {
   const KeysToMS: Duration = {
-    "years": 31556952000,
-    "months": 2629800000,
-    "days": 86400000,
-    "hours": 3600000,
-    "minutes": 60000,
-    "seconds": 1000,
+    years: 31556952000,
+    months: 2629800000,
+    days: 86400000,
+    hours: 3600000,
+    minutes: 60000,
+    seconds: 1000,
   };
   let durationMilli = 0;
   let durationObj = parse(isoDuration);
@@ -40,4 +52,5 @@ export const convertISODurationToMS = (isoDuration: string) => {
   return durationMilli;
 };
 
-export const captalizeName = (name: string) => name.charAt(0).toUpperCase() + name.slice(1);
+export const captalizeName = (name: string) =>
+  name.charAt(0).toUpperCase() + name.slice(1);
