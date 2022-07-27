25aeb829beb2a4c4009b0d8511d4612635ed6128
Danilo Arantes <dfilho@purdue.edu>
2022-07-27 12:30:14 -0300

    config file fix

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 export const ENVIRONMENT = process.env.NODE_ENV ?? 'development';
 export const PORT = 8000;
-export const CLIENT_URL = "http://localhost:3000";
+export const CLIENT_URL = process.env.CLIENT_URL;
 export const DISCORD_API_BASE_URL = "https://discord.com/api";
 export const CORS_ORIGINS = [CLIENT_URL];
 // Time in ms where users are not allowed to send more than one
